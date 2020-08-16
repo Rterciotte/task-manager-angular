83d1a5f34a8285d1db2b1367c2a5328eec44e8bd
Rogério Terciotte <rterciotte@gmail.com>
2020-08-16 16:51:19 -0300

    Adding tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gerenciador de Tarefas';
+}
+
+export class Task{
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
 }
